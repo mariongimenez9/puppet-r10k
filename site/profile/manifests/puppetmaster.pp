cec355cb1e525412095826cee7540f150b2e7b61
gimenez marion <gimenez marion>
2016-10-21 09:55:57 +0200

    file_line for master

--- site/profile/manifests/puppetmaster.pp
+++ site/profile/manifests/puppetmaster.pp
@@ -42,13 +42,40 @@ class profile::puppetmaster(
   # Service['puppetserver']->Service['puppet']
   
   if $isnt_ca {
-  file { "/etc/puppetlabs/puppetserver/services.d/ca.cfg":
+  #file { "/etc/puppetlabs/puppetserver/services.d/ca.cfg":
+  #	ensure => file,
+  #	content => epp('profile/puppetmaster/ca.cfg.epp',{ 'ca' => hiera('profiles::puppetmaster::ca',[])}),
+  #	}
+  #file { "/etc/puppetlabs/puppetserver/conf.d/webserver.conf":
+  #	ensure => file,
+  #	content => epp('profile/puppetmaster/webserver.conf.epp',{ 'webserver' => hiera('profiles::puppetmaster::webserver',[])}),
+  #	}
+  file_line { 'ca.cfg':
 	ensure => file,
-	content => epp('profile/puppetmaster/ca.cfg.epp',{ 'ca' => hiera('profiles::puppetmaster::ca',[])}),
+	path => '/etc/puppetlabs/puppetserver/services.d/ca.cfg',
+	line => '#puppetlabs.services.ca.certificate-authority-disabled-service/certificate-authority-disabled-service',
+	match => '^#puppetlabs.services.ca.certificate-authority-disabled-service/certificate-authority-disabled-service',
 	}
-  file { "/etc/puppetlabs/puppetserver/conf.d/webserver.conf":
+  file_line { 'ca.cfg':
 	ensure => file,
-	content => epp('profile/puppetmaster/webserver.conf.epp',{ 'webserver' => hiera('profiles::puppetmaster::webserver',[])}),
+	path => '/etc/puppetlabs/puppetserver/services.d/ca.cfg',
+	line => '#puppetlabs.services.ca.certificate-authority-service/certificate-authority-service',
+	match => '^puppetlabs.services.ca.certificate-authority-service/certificate-authority-service',
+	}
+  file_line { 'webserver.conf':
+	ensure => file,
+	path => '/etc/puppetlabs/puppetserver/conf.d/webserver.conf',
+	line => 'ssl-cert: /etc/puppetlabs/puppet/ssl/certs/puppetmaster2.vm.local.pem',
+	}
+  file_line { 'webserver.conf':
+	ensure => file,
+	path => '/etc/puppetlabs/puppetserver/conf.d/webserver.conf',
+	line => 'ssl-key: /etc/puppetlabs/puppet/ssl/private_keys/puppetmaster2.vm.local.pem',
+	}
+  file_line { 'webserver.conf':
+	ensure => file,
+	path => '/etc/puppetlabs/puppetserver/conf.d/webserver.conf',
+	line => 'ssl-ca-cert: /etc/puppetlabs/puppet/ssl/certs/ca.pem',
 	}
   }
 
